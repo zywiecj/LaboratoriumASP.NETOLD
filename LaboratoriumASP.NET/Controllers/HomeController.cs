c3ce89e3bc44a9c02dd3d87c5b460ac30742443a
ribci <ribciak@gmail.com>
2024-10-09 12:13:22 +0200

    Skonczone

--- LaboratoriumASP.NET/Controllers/HomeController.cs
+++ LaboratoriumASP.NET/Controllers/HomeController.cs
@@ -12,6 +12,13 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    
+    
+    /*
+     * Utworz metode Calculator oraz widok w nim wyswietl tylko napis Kalkulator
+     * Dodaj link w nawigacji aplikacji do metody Kalkulator
+     */
+    
 
     public IActionResult Index()
     {
@@ -27,6 +34,52 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Calculator(Operator? op, double? x, double? y)
+    {
+        //https://localhost:7019/Home/Calculator?op=add&x=4&y=1,5
+        // var op = Request.Query["op"];
+        // var x = double.Parse(Request.Query["x"]);
+        // // var y= double.Parse(Request.Query["y"]);
+        if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
+            return View("CalculatorError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CalculatorError");
+        }
+        
+        double? result = 0.0;
+        switch (op)
+        {
+            case Operator.Add:
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case Operator.Sub:
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case Operator.Mul:
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            case Operator.Div:
+                result = x / y;
+                ViewBag.Operator = ":";
+                break;
+            
+        }
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
+        return View();
+        
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
@@ -34,3 +87,13 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
+
+public enum Operator
+{
+    Add, Sub, Mul, Div
+}
+
+/*  Zadanie domowe 
+* Napisz metode Age, ktora przyjmuje parametry z datą urodzin i wyświetla wiek
+* w latach, miesiącach i dniach.
+*/
