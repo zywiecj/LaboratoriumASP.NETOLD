9ccdbbf692d85e02133c68b3b1b5eb8f4a0bbaa2
ribci <ribciak@gmail.com>
2024-10-14 22:43:30 +0200

    ageadded

--- LaboratoriumASP.NET/Controllers/HomeController.cs
+++ LaboratoriumASP.NET/Controllers/HomeController.cs
@@ -2,98 +2,111 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using LaboratoriumASP.NET.Models;
 
-namespace LaboratoriumASP.NET.Controllers;
-
-public class HomeController : Controller
+namespace LaboratoriumASP.NET.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-    
-    
-    /*
-     * Utworz metode Calculator oraz widok w nim wyswietl tylko napis Kalkulator
-     * Dodaj link w nawigacji aplikacji do metody Kalkulator
-     */
-    
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-    
-    public IActionResult About()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-    
-    public IActionResult Calculator(Operator? op, double? x, double? y)
-    {
-        //https://localhost:7019/Home/Calculator?op=add&x=4&y=1,5
-        // var op = Request.Query["op"];
-        // var x = double.Parse(Request.Query["x"]);
-        // // var y= double.Parse(Request.Query["y"]);
-        if (x is null || y is null)
+        public HomeController(ILogger<HomeController> logger)
         {
-            ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
-            return View("CalculatorError");
+            _logger = logger;
         }
 
-        if (op is null)
+        public IActionResult Index()
         {
-            ViewBag.ErrorMessage = "Nieznany operator!";
-            return View("CalculatorError");
+            return View();
         }
-        
-        double? result = 0.0;
-        switch (op)
+
+        public IActionResult About()
         {
-            case Operator.Add:
-                result = x + y;
-                ViewBag.Operator = "+";
-                break;
-            case Operator.Sub:
-                result = x - y;
-                ViewBag.Operator = "-";
-                break;
-            case Operator.Mul:
-                result = x * y;
-                ViewBag.Operator = "*";
-                break;
-            case Operator.Div:
-                result = x / y;
-                ViewBag.Operator = ":";
-                break;
-            
+            return View();
         }
-        ViewBag.Result = result;
-        ViewBag.X = x;
-        ViewBag.Y = y;
-        return View();
-        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator(Operator? op, double? x, double? y)
+        {
+            if (x is null || y is null)
+            {
+                ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
+                return View("CalculatorError");
+            }
+
+            if (op is null)
+            {
+                ViewBag.ErrorMessage = "Nieznany operator!";
+                return View("CalculatorError");
+            }
+
+            double? result = 0.0;
+            switch (op)
+            {
+                case Operator.Add:
+                    result = x + y;
+                    ViewBag.Operator = "+";
+                    break;
+                case Operator.Sub:
+                    result = x - y;
+                    ViewBag.Operator = "-";
+                    break;
+                case Operator.Mul:
+                    result = x * y;
+                    ViewBag.Operator = "*";
+                    break;
+                case Operator.Div:
+                    result = x / y;
+                    ViewBag.Operator = ":";
+                    break;
+            }
+            ViewBag.Result = result;
+            ViewBag.X = x;
+            ViewBag.Y = y;
+            return View();
+        }
+
+        // Dodaj nową metodę Age
+        [HttpGet]
+        public IActionResult Age(DateTime? birthDate)
+        {
+            if (birthDate.HasValue)
+            {
+                // Oblicz aktualną datę
+                DateTime today = DateTime.Now;
+
+                // Oblicz różnicę w latach, miesiącach i dniach
+                int years = today.Year - birthDate.Value.Year;
+                int months = today.Month - birthDate.Value.Month;
+                int days = today.Day - birthDate.Value.Day;
+
+                // Korekta na dni
+                if (days < 0)
+                {
+                    months--;
+                    days += DateTime.DaysInMonth(today.Year, today.Month - 1);
+                }
+
+                // Korekta na miesiące
+                if (months < 0)
+                {
+                    years--;
+                    months += 12;
+                }
+
+                // Przekazujemy wynik do widoku za pomocą ViewBag
+                ViewBag.AgeResult = $"{years} lat, {months} miesięcy, {days} dni";
+                ViewBag.BirthDate = birthDate.Value.ToShortDateString();
+            }
+
+            return View();
+        }
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public enum Operator
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        Add, Sub, Mul, Div
     }
 }
-
-public enum Operator
-{
-    Add, Sub, Mul, Div
-}
-
-/*  Zadanie domowe 
-* Napisz metode Age, ktora przyjmuje parametry z datą urodzin i wyświetla wiek
-* w latach, miesiącach i dniach.
-*/
